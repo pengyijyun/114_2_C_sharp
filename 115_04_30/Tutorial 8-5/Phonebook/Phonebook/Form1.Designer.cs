b2472ad672755aa148ab919c93bd316045f50dcb
Yi Jyun Peng <a113221252@mail.shu.edu.tw>
2026-04-30 15:46:54 +0800

    115_04_30

---
+++ 115_04_30/Tutorial 8-5/Phonebook/Phonebook/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Phonebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的設計工具容器變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受管理資源，為 true；否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 表單設計工具產生的程式碼
+
+        /// <summary>
+        /// 設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.selectedPhoneDescriptionLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(36, 25);
+            this.promptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(269, 40);
+            this.promptLabel.TabIndex = 0;
+            this.promptLabel.Text = "請選擇一個姓名";
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.ItemHeight = 40;
+            this.nameListBox.Location = new System.Drawing.Point(33, 69);
+            this.nameListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(413, 284);
+            this.nameListBox.TabIndex = 1;
+            this.nameListBox.SelectedIndexChanged += new System.EventHandler(this.nameListBox_SelectedIndexChanged);
+            // 
+            // selectedPhoneDescriptionLabel
+            // 
+            this.selectedPhoneDescriptionLabel.AutoSize = true;
+            this.selectedPhoneDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectedPhoneDescriptionLabel.Location = new System.Drawing.Point(528, 149);
+            this.selectedPhoneDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.selectedPhoneDescriptionLabel.Name = "selectedPhoneDescriptionLabel";
+            this.selectedPhoneDescriptionLabel.Size = new System.Drawing.Size(161, 40);
+            this.selectedPhoneDescriptionLabel.TabIndex = 2;
+            this.selectedPhoneDescriptionLabel.Text = "電話號碼";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneLabel.Location = new System.Drawing.Point(493, 204);
+            this.phoneLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(229, 66);
+            this.phoneLabel.TabIndex = 3;
+            this.phoneLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(296, 408);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(150, 62);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "結束";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 500);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.selectedPhoneDescriptionLabel);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.promptLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "電話簿";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Label selectedPhoneDescriptionLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
