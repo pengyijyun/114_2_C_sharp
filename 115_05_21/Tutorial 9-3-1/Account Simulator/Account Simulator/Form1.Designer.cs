b1d104e788291253f83d87f5a74f7bee029dc878
Yi Jyun Peng <a113221252@mail.shu.edu.tw>
2026-05-21 16:04:12 +0800

    115_05_21

---
+++ 115_05_21/Tutorial 9-3-1/Account Simulator/Account Simulator/Form1.Designer.cs
@@ -0,0 +1,323 @@
+namespace Account_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.depositGroupBox = new System.Windows.Forms.GroupBox();
+            this.depositButton = new System.Windows.Forms.Button();
+            this.depositTextBox = new System.Windows.Forms.TextBox();
+            this.depositAmountDescriptionLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.withdrawButton = new System.Windows.Forms.Button();
+            this.withdrawTextBox = new System.Windows.Forms.TextBox();
+            this.withdrawAmountDescriptionLabel = new System.Windows.Forms.Label();
+            this.balanceDescriptionLabel = new System.Windows.Forms.Label();
+            this.balanceLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.depositGroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // depositGroupBox
+            // 
+            this.depositGroupBox.Controls.Add(this.depositButton);
+            this.depositGroupBox.Controls.Add(this.depositTextBox);
+            this.depositGroupBox.Controls.Add(this.depositAmountDescriptionLabel);
+            this.depositGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.depositGroupBox.Location = new System.Drawing.Point(31, 433);
+            this.depositGroupBox.Margin = new System.Windows.Forms.Padding(4);
+            this.depositGroupBox.Name = "depositGroupBox";
+            this.depositGroupBox.Padding = new System.Windows.Forms.Padding(4);
+            this.depositGroupBox.Size = new System.Drawing.Size(300, 172);
+            this.depositGroupBox.TabIndex = 0;
+            this.depositGroupBox.TabStop = false;
+            this.depositGroupBox.Text = "存款";
+            // 
+            // depositButton
+            // 
+            this.depositButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.depositButton.Location = new System.Drawing.Point(139, 103);
+            this.depositButton.Margin = new System.Windows.Forms.Padding(4);
+            this.depositButton.Name = "depositButton";
+            this.depositButton.Size = new System.Drawing.Size(126, 61);
+            this.depositButton.TabIndex = 2;
+            this.depositButton.Text = "存款";
+            this.depositButton.UseVisualStyleBackColor = true;
+            this.depositButton.Click += new System.EventHandler(this.depositButton_Click);
+            // 
+            // depositTextBox
+            // 
+            this.depositTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.depositTextBox.Location = new System.Drawing.Point(129, 40);
+            this.depositTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.depositTextBox.Name = "depositTextBox";
+            this.depositTextBox.Size = new System.Drawing.Size(148, 48);
+            this.depositTextBox.TabIndex = 1;
+            // 
+            // depositAmountDescriptionLabel
+            // 
+            this.depositAmountDescriptionLabel.AutoSize = true;
+            this.depositAmountDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.depositAmountDescriptionLabel.Location = new System.Drawing.Point(17, 48);
+            this.depositAmountDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.depositAmountDescriptionLabel.Name = "depositAmountDescriptionLabel";
+            this.depositAmountDescriptionLabel.Size = new System.Drawing.Size(125, 40);
+            this.depositAmountDescriptionLabel.TabIndex = 0;
+            this.depositAmountDescriptionLabel.Text = "金額：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.withdrawButton);
+            this.groupBox1.Controls.Add(this.withdrawTextBox);
+            this.groupBox1.Controls.Add(this.withdrawAmountDescriptionLabel);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.groupBox1.Location = new System.Drawing.Point(339, 433);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(300, 172);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "提款";
+            // 
+            // withdrawButton
+            // 
+            this.withdrawButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.withdrawButton.Location = new System.Drawing.Point(155, 103);
+            this.withdrawButton.Margin = new System.Windows.Forms.Padding(4);
+            this.withdrawButton.Name = "withdrawButton";
+            this.withdrawButton.Size = new System.Drawing.Size(122, 61);
+            this.withdrawButton.TabIndex = 2;
+            this.withdrawButton.Text = "提款";
+            this.withdrawButton.UseVisualStyleBackColor = true;
+            this.withdrawButton.Click += new System.EventHandler(this.withdrawButton_Click);
+            // 
+            // withdrawTextBox
+            // 
+            this.withdrawTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.withdrawTextBox.Location = new System.Drawing.Point(137, 45);
+            this.withdrawTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.withdrawTextBox.Name = "withdrawTextBox";
+            this.withdrawTextBox.Size = new System.Drawing.Size(155, 48);
+            this.withdrawTextBox.TabIndex = 1;
+            // 
+            // withdrawAmountDescriptionLabel
+            // 
+            this.withdrawAmountDescriptionLabel.AutoSize = true;
+            this.withdrawAmountDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.withdrawAmountDescriptionLabel.Location = new System.Drawing.Point(21, 48);
+            this.withdrawAmountDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.withdrawAmountDescriptionLabel.Name = "withdrawAmountDescriptionLabel";
+            this.withdrawAmountDescriptionLabel.Size = new System.Drawing.Size(125, 40);
+            this.withdrawAmountDescriptionLabel.TabIndex = 0;
+            this.withdrawAmountDescriptionLabel.Text = "金額：";
+            // 
+            // balanceDescriptionLabel
+            // 
+            this.balanceDescriptionLabel.AutoSize = true;
+            this.balanceDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.balanceDescriptionLabel.Location = new System.Drawing.Point(707, 358);
+            this.balanceDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.balanceDescriptionLabel.Name = "balanceDescriptionLabel";
+            this.balanceDescriptionLabel.Size = new System.Drawing.Size(197, 40);
+            this.balanceDescriptionLabel.TabIndex = 2;
+            this.balanceDescriptionLabel.Text = "帳戶資訊：";
+            // 
+            // balanceLabel
+            // 
+            this.balanceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.balanceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.balanceLabel.Location = new System.Drawing.Point(665, 410);
+            this.balanceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.balanceLabel.Name = "balanceLabel";
+            this.balanceLabel.Size = new System.Drawing.Size(290, 220);
+            this.balanceLabel.TabIndex = 3;
+            this.balanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.exitButton.Location = new System.Drawing.Point(731, 634);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(150, 62);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "結束";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.groupBox2.Location = new System.Drawing.Point(36, 40);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(654, 366);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "建立帳戶";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox4.Location = new System.Drawing.Point(458, 295);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(184, 63);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "建立帳戶";
+            this.textBox4.UseVisualStyleBackColor = true;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox3.Location = new System.Drawing.Point(283, 239);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(278, 48);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label3.Location = new System.Drawing.Point(78, 239);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "開戶金額：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox2.Location = new System.Drawing.Point(283, 165);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(278, 48);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label2.Location = new System.Drawing.Point(150, 168);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "姓名：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox1.Location = new System.Drawing.Point(283, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 48);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(150, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "帳號：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 709);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.balanceLabel);
+            this.Controls.Add(this.balanceDescriptionLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.depositGroupBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "帳戶模擬器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.depositGroupBox.ResumeLayout(false);
+            this.depositGroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox depositGroupBox;
+        private System.Windows.Forms.Button depositButton;
+        private System.Windows.Forms.TextBox depositTextBox;
+        private System.Windows.Forms.Label depositAmountDescriptionLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button withdrawButton;
+        private System.Windows.Forms.TextBox withdrawTextBox;
+        private System.Windows.Forms.Label withdrawAmountDescriptionLabel;
+        private System.Windows.Forms.Label balanceDescriptionLabel;
+        private System.Windows.Forms.Label balanceLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button textBox4;
+    }
+}
+
---
+++ 115_05_21/Tutorial 9-4/Cell Phone Inventory/Cell Phone Inventory/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Cell_Phone_Inventory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterDataGroupBox = new System.Windows.Forms.GroupBox();
+            this.addPhoneButton = new System.Windows.Forms.Button();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.modelTextBox = new System.Windows.Forms.TextBox();
+            this.brandTextBox = new System.Windows.Forms.TextBox();
+            this.promptPriceLabel = new System.Windows.Forms.Label();
+            this.promptModelLabel = new System.Windows.Forms.Label();
+            this.promptBrandLabel = new System.Windows.Forms.Label();
+            this.listGroupBox = new System.Windows.Forms.GroupBox();
+            this.phoneListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.enterDataGroupBox.SuspendLayout();
+            this.listGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // enterDataGroupBox
+            // 
+            this.enterDataGroupBox.Controls.Add(this.addPhoneButton);
+            this.enterDataGroupBox.Controls.Add(this.priceTextBox);
+            this.enterDataGroupBox.Controls.Add(this.modelTextBox);
+            this.enterDataGroupBox.Controls.Add(this.brandTextBox);
+            this.enterDataGroupBox.Controls.Add(this.promptPriceLabel);
+            this.enterDataGroupBox.Controls.Add(this.promptModelLabel);
+            this.enterDataGroupBox.Controls.Add(this.promptBrandLabel);
+            this.enterDataGroupBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.enterDataGroupBox.Location = new System.Drawing.Point(18, 15);
+            this.enterDataGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.enterDataGroupBox.Name = "enterDataGroupBox";
+            this.enterDataGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.enterDataGroupBox.Size = new System.Drawing.Size(486, 346);
+            this.enterDataGroupBox.TabIndex = 5;
+            this.enterDataGroupBox.TabStop = false;
+            this.enterDataGroupBox.Text = "輸入手機資料";
+            // 
+            // addPhoneButton
+            // 
+            this.addPhoneButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.addPhoneButton.Location = new System.Drawing.Point(161, 270);
+            this.addPhoneButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.addPhoneButton.Name = "addPhoneButton";
+            this.addPhoneButton.Size = new System.Drawing.Size(190, 55);
+            this.addPhoneButton.TabIndex = 7;
+            this.addPhoneButton.Text = "新增手機";
+            this.addPhoneButton.UseVisualStyleBackColor = true;
+            this.addPhoneButton.Click += new System.EventHandler(this.addPhoneButton_Click);
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.priceTextBox.Location = new System.Drawing.Point(195, 194);
+            this.priceTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(268, 55);
+            this.priceTextBox.TabIndex = 5;
+            // 
+            // modelTextBox
+            // 
+            this.modelTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.modelTextBox.Location = new System.Drawing.Point(195, 125);
+            this.modelTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.modelTextBox.Name = "modelTextBox";
+            this.modelTextBox.Size = new System.Drawing.Size(268, 55);
+            this.modelTextBox.TabIndex = 4;
+            // 
+            // brandTextBox
+            // 
+            this.brandTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.brandTextBox.Location = new System.Drawing.Point(195, 55);
+            this.brandTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.brandTextBox.Name = "brandTextBox";
+            this.brandTextBox.Size = new System.Drawing.Size(268, 55);
+            this.brandTextBox.TabIndex = 3;
+            // 
+            // promptPriceLabel
+            // 
+            this.promptPriceLabel.AutoSize = true;
+            this.promptPriceLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.promptPriceLabel.Location = new System.Drawing.Point(60, 201);
+            this.promptPriceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptPriceLabel.Name = "promptPriceLabel";
+            this.promptPriceLabel.Size = new System.Drawing.Size(100, 45);
+            this.promptPriceLabel.TabIndex = 2;
+            this.promptPriceLabel.Text = "價格:";
+            // 
+            // promptModelLabel
+            // 
+            this.promptModelLabel.AutoSize = true;
+            this.promptModelLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.promptModelLabel.Location = new System.Drawing.Point(60, 132);
+            this.promptModelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptModelLabel.Name = "promptModelLabel";
+            this.promptModelLabel.Size = new System.Drawing.Size(100, 45);
+            this.promptModelLabel.TabIndex = 1;
+            this.promptModelLabel.Text = "型號:";
+            // 
+            // promptBrandLabel
+            // 
+            this.promptBrandLabel.AutoSize = true;
+            this.promptBrandLabel.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.promptBrandLabel.Location = new System.Drawing.Point(60, 62);
+            this.promptBrandLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.promptBrandLabel.Name = "promptBrandLabel";
+            this.promptBrandLabel.Size = new System.Drawing.Size(100, 45);
+            this.promptBrandLabel.TabIndex = 0;
+            this.promptBrandLabel.Text = "品牌:";
+            // 
+            // listGroupBox
+            // 
+            this.listGroupBox.Controls.Add(this.phoneListBox);
+            this.listGroupBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.listGroupBox.Location = new System.Drawing.Point(530, 15);
+            this.listGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listGroupBox.Name = "listGroupBox";
+            this.listGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listGroupBox.Size = new System.Drawing.Size(635, 346);
+            this.listGroupBox.TabIndex = 8;
+            this.listGroupBox.TabStop = false;
+            this.listGroupBox.Text = "選擇手機";
+            // 
+            // phoneListBox
+            // 
+            this.phoneListBox.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.phoneListBox.FormattingEnabled = true;
+            this.phoneListBox.ItemHeight = 45;
+            this.phoneListBox.Location = new System.Drawing.Point(30, 55);
+            this.phoneListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.phoneListBox.Name = "phoneListBox";
+            this.phoneListBox.Size = new System.Drawing.Size(573, 274);
+            this.phoneListBox.TabIndex = 8;
+            this.phoneListBox.SelectedIndexChanged += new System.EventHandler(this.phoneListBox_SelectedIndexChanged);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F);
+            this.exitButton.Location = new System.Drawing.Point(383, 389);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(262, 55);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "結束";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 468);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.listGroupBox);
+            this.Controls.Add(this.enterDataGroupBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "手機庫存";
+            this.enterDataGroupBox.ResumeLayout(false);
+            this.enterDataGroupBox.PerformLayout();
+            this.listGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox enterDataGroupBox;
+        private System.Windows.Forms.Button addPhoneButton;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox modelTextBox;
+        private System.Windows.Forms.TextBox brandTextBox;
+        private System.Windows.Forms.Label promptPriceLabel;
+        private System.Windows.Forms.Label promptModelLabel;
+        private System.Windows.Forms.Label promptBrandLabel;
+        private System.Windows.Forms.GroupBox listGroupBox;
+        private System.Windows.Forms.ListBox phoneListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
